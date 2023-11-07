1eb04de10fb0f0bc18eaeba0dde9b43af9673a6f
Lordwill <messagelordwill@gmail.com>
2023-11-07 05:32:22 -0800

    initial commmit

--- src/app/shared/constants/expense-constants.ts
+++
@@ -1,2 +0,0 @@
-export const expense_categories: string[] = ['Groceries', 'Transportation', 'Entertainment', 'Dining out', 'Unassigned'];
-export const expense_types: string[] = ['Credit', 'Debit', 'Cash'];
